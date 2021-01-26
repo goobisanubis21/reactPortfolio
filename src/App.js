84ead2ffc27b96de0999bf87a66f8181d0476bc2
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-25 22:24:57 -0500

    starting off with place holder text for nav bar and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Nav/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <h1>nav</h1>
+    );
+}
+
+export default Nav;
