469252142c52afe36b1e3b5b7b1c26255b2835e6
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-27 15:20:41 -0500

    added resume link to homepage

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import github from "../../static/images/git.png";
 import linkedin from "../../static/images/linked.png";
 import instagram from "../../static/images/instalogo.png";
+import resume from "../../static/images/resume.png";
 import "./homeCard.css";
 
 function HomeCard() {
@@ -13,12 +14,16 @@ function HomeCard() {
                 <br />
                 <h5 className="card-subtitle mb-2 text-muted">- HTML - CSS - JavaScript - JQuery - ReactJS - NodeJS - MONGOD - MySQL -</h5>
                 <br />
-                <strong className="card-text">Currently I am enrolled in the Rutgers bootcamp curriculum for fullstack software development. I started coding on my own in 2018 and completed the CS50 introduction to computer science course through Harvard University in early 2020. I prefer working as a fullstack developer with ReactJS, I enjoy the backend development as much as the frontend. I feel very confortable with my UI design capabilities due to my highly skilled artistic background in oil painting, the Instagram link below has a lot of my paintings for display. I look forward to a life long career in web design understanding the fact that learning something new each day will only bring me closer to my goal of mastering my development skills.</strong>
+                <strong id="card-text" className="card-text">Currently I am enrolled in the Rutgers bootcamp curriculum for fullstack software development. I started coding on my own in 2018 and completed the CS50 introduction to computer science course through Harvard University in early 2020. I prefer working as a fullstack developer with ReactJS, I enjoy the backend development as much as the frontend. I feel very confortable with my UI design capabilities due to my highly skilled artistic background in oil painting, the Instagram link below has a lot of my paintings for display. I look forward to a life long career in web design understanding the fact that learning something new each day will only bring me closer to my goal of mastering my development skills.</strong>
+                <br />
+                <br />
                 <br />
                 <div id="logos">
                     <a href="https://github.com/goobisanubis21" className="card-link"><img id="githubLogo" src={github} alt="github" /></a>
                     <a href="https://www.linkedin.com/in/anthony-parrino-887b391b3/" className="card-link"><img id="linkedinLogo" src={linkedin} alt="linkedin" /></a>
                     <a href="https://www.instagram.com/antonio21_art/" className="card-link"><img id="instagramLogo" src={instagram} alt="instagram" /></a>
+                    <a href="https://docs.google.com/document/d/1DF1pCoxE6CbxVXvTtMoAfvGEWmGFIUaM0rdCEHtcqZw/edit?usp=sharing" className="card-link"><img id="resumeLogo" src={resume} alt="resume" /></a>
+                    
                 </div>
             </div>
         </div>
