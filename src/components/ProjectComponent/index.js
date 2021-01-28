fcfabb64b06c34765f9b03af0dc5460ae55e190e
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-28 12:27:43 -0500

    creating project file, debugging image rendering

--- src/components/ProjectComponent/index.js
+++ src/components/ProjectComponent/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function ProjectComponent() {
+function ProjectComponent(props) {
     return (
-        <div>
-            hi
+        <div className="card">
+            <h2 className="content">{props.title}</h2>
+            <div className="img-container">
+                <img src={props.image} alt={props.title} />
+            </div>
+            <div className="content">
+                <li>{props.description}</li>
+                <li><a href={props.link}>Github Code</a></li>
+                <li><a href={props.code}>Project Link</a></li>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectComponent
+export default ProjectComponent;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ProjectComponent from "../components/ProjectComponent"
+import projects from "../projects.json";
 
 function Projects() {
+
     return(
         <div>
-            <ProjectComponent />
+            <h1 className="projectsTitle">Projects</h1>
+            {projects.map(project => (
+                <ProjectComponent 
+                    key={project.id}
+                    title={project.title}
+                    image={project.image}
+                    description={project.description}
+                    link={project.link}
+                    code={project.code}
+                />
+            ))}
         </div>
     )
 }
