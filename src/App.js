6516efda4f7bacfeb4a14639c9344c98267c3101
Anthony Parrino <anthony21parrino21@gmail.com>
2021-01-30 10:18:46 -0500

    making changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <div>
             <Nav />
             <Header />
