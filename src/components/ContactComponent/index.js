9781192f4be23fd8295a8cd1fa318219a404414b
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-23 14:29:04 -0500

    created working email

--- src/components/ContactComponent/index.js
+++ src/components/ContactComponent/index.js
@@ -1,38 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import "./contactComp.css"
+import React from 'react'
+import "./contactComp.css";
+import emailjs from "emailjs-com";
 
 
 function ContactComponent() {
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [description, setDescription] = useState("");
+    function sendEmail(e) {
+        e.preventDefault();
 
-    useEffect(() => {
-        console.log(firstName + lastName + email + description)
-    });
-
-    const thanks = (e) => {
-        e.preventDefault()
-        alert("Your email has been sent and will be answered ASAP! thanks " + firstName)
+        emailjs.sendForm('service_5zi8uuf', 'template_u0bc2yx', e.target, 'user_I9bocqPBOHKJVRBscJGav')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     }
 
     return (
         <div id="contactPage">
             <h1 id="contact">Contact Me</h1>
-            <form id="formCard" onSubmit={thanks}>
+            <form id="formCard" onSubmit={sendEmail}>
                 <div className="inputBorder">
-                    <input className="inputEl" type="text" placeholder="First Name" name="firstName" value={firstName} onChange={(e) =>setFirstName(e.target.value)}></input>
+                    <input className="inputEl" type="text" placeholder="First Name" name="firstName"></input>
                 </div>
                 <div className="inputBorder">
-                    <input className="inputEl" type="text" placeholder="Last Name" name="lastName" value={lastName} onChange={(e) =>setLastName(e.target.value)}></input>
+                    <input className="inputEl" type="text" placeholder="Last Name" name="lastName"></input>
                 </div>
                 <div className="inputBorder">
-                    <input className="inputEl" type="text" placeholder="Email" name="email" value={email} onChange={(e) =>setEmail(e.target.value)} ></input>
+                    <input className="inputEl" type="text" placeholder="Email" name="email"></input>
                 </div>
-                <textarea className="textEl" type="text" placeholder="Description" name="description" value={description} onChange={(e) =>setDescription(e.target.value)}></textarea>
-                <button className="subBtn" type="submit" onSubmit={() => test}>Send</button>
+                <textarea className="textEl" type="text" placeholder="Description" name="description"></textarea>
+                <button className="subBtn" type="submit">Send</button>
             </form>
             <p id="privacy">All contact information is private and will never be displayed publicly</p>
         </div>
